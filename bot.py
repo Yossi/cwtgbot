87c8f08ff7d9f3dfb65d3a4496635ff253b41cf5
Yossi Rafelson <yossi.rafelson@gmail.com>
2019-12-17 19:40:28 -0800

    Fill out /help some more with new functionality

--- bot.py
+++ bot.py
@@ -168,10 +168,12 @@ def help(update, context):
            'Any list of items (eg. /stock, ⚒Crafting, etc.) - Create all the /g_deposit commands to move everything to guild stock.\n'\
            'Any message with list of missing items - Create /g_withdraw command that you need to send to guild leader.\n'\
            'A message with /g_deposit commands from this bot - Create /g_withdraw command to get your stuff back out.\n'\
+           'Any message that has your guild tag in it - Record you as a member of that guild.\n'\
+           'Paste in multiple /g_withdraw commands - Create a more efficent /g_withdraw command.\n'\
+           '/warehouse - Show a list of things that have enough recipies & parts in guild stock to craft.\n'\
+           '/stock & /alch - List guild stock of these categories in decending order of quantity.\n'\
            '\n'\
            'Some /settings available too.\n'\
-           '\n'\
-           'Paste in multiple /g_withdraw commands - Create a more efficent /g_withdraw command.'
 
     logging.info(f'bot said:\n{text}')
     context.bot.send_message(chat_id=update.message.chat_id, text=text, parse_mode=ParseMode.HTML)
