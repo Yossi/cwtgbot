fee5157d222d244dc3bfbba6aa3284fc91a0e908
Yossi Rafelson <yossi.rafelson@gmail.com>
2020-01-19 02:59:09 -0800

    Whitespace changes

--- brains.py
+++ brains.py
@@ -340,11 +340,13 @@ def warehouse_crafting(context):
     responses = []
     now = datetime.datetime.utcnow()
     if (rec := warehouse.get('rec', {})) and (parts := warehouse.get('parts', {})) and \
-    (age_rec := now - rec['timestamp']) < datetime.timedelta(hours=hours) and (age_parts := now - parts['timestamp']) < datetime.timedelta(hours=hours):
+    (age_rec := now - rec['timestamp']) < datetime.timedelta(hours=hours) and \
+    (age_parts := now - parts['timestamp']) < datetime.timedelta(hours=hours):
         older_command = '/g_stock_parts' if age_parts >= age_rec else '/g_stock_rec'
         output = [f'Based on {older_command} data {max(age_rec, age_parts).seconds // 60} minutes old:\n']
         page_counter = 0
         for n in range(1,103):
+
             if 1 <= n <= 18 or 59 <= n <= 61:
                 parts_needed = 3
             elif 100 <= n <= 102:
