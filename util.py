8cdccb7c3faab4d70ca019ba4918d757fa9f5ff1
Yossi Rafelson <yossi.rafelson@gmail.com>
2020-03-04 13:40:20 -0800

    Add what qualifications are needed to find this loot
    Close #29

--- util.py
+++ util.py
@@ -45,7 +45,22 @@ def get_id_location(id):
     for place, emoji in locations:
         if info.get(f'drop{place}{phase}'):
             output += emoji
+    if output: output += ' ' + get_qualifications(id)
     return output
+
+def get_qualifications(id):
+    levels = {
+        22: '22≤🏅≤39',
+        32: '32≤🏅≤39',
+        40: '40≤🏅≤45',
+        46: '46≤🏅'
+    }
+    level = levels.get(id_lookup.get(id, {}).get('questMinLevel'), '')
+    roman = ['', '<code>I/II</code>', '<code>II</code>', '<code>III</code>', '<code>IV</code>']
+    perception_level = roman[id_lookup.get(id, {}).get('questPerceptionLevel', 0)]
+    if perception_level: perception_level = '👀' + perception_level
+
+    return level + perception_level
 
 def warehouse_load_saved(ignore_exceptions=True, guild='full'):
     try:
