0cb2400d2b054fcdde15efa2bee7a25678ad0f50
Yossi Rafelson <yossi.rafelson@gmail.com>
2020-04-06 22:43:35 -0700

    Organize util.py slightly

--- util.py
+++ util.py
@@ -17,6 +17,33 @@ def scrape_data(fp):
     data = requests.get(url).json()['items']
     pickle.dump(data, fp)
 
+def get_lookup_dicts():
+    try:
+        lastrev = requests.get('https://raw.githubusercontent.com/AVee/cw_wiki_sync/master/data/lastrev').text
+
+        with open('lastrev', 'r') as revfp:
+            localrev = revfp.readline()
+            if lastrev != localrev:
+                with open('data.dict', 'wb') as fp:
+                    scrape_data(fp)
+
+        with open('lastrev', 'w') as fp:
+            fp.write(lastrev)
+    except:
+        pass # if we end up here for whatever reason (almost certainly a network error) then just move on. If network is really down AND we don't have data.dict yet then we explode later
+
+    if not Path('data.dict').is_file():
+        with open('data.dict', 'wb') as fp:
+            scrape_data(fp)
+    with open('data.dict', 'rb') as fp:
+        data = pickle.load(fp)
+        id_lookup = {}
+        name_lookup = {}
+        for item in data:
+            id_lookup[item['id']] = item
+            name_lookup[item['name'].lower()] = item
+    return id_lookup, name_lookup
+
 def is_witching_hour():
     '''return True if market is closed'''
     closed_times = (
@@ -72,33 +99,6 @@ def warehouse_load_saved(guild='full'):
 
     except IOError:
         return {}  # Ignore if warehouse.dict doesn't exist or can't be opened.
-
-def get_lookup_dicts():
-    try:
-        lastrev = requests.get('https://raw.githubusercontent.com/AVee/cw_wiki_sync/master/data/lastrev').text
-
-        with open('lastrev', 'r') as revfp:
-            localrev = revfp.readline()
-            if lastrev != localrev:
-                with open('data.dict', 'wb') as fp:
-                    scrape_data(fp)
-
-        with open('lastrev', 'w') as fp:
-            fp.write(lastrev)
-    except:
-        pass # if we end up here for whatever reason (almost certainly a network error) then just move on. If network is really down AND we don't have data.dict yet then we explode later
-
-    if not Path('data.dict').is_file():
-        with open('data.dict', 'wb') as fp:
-            scrape_data(fp)
-    with open('data.dict', 'rb') as fp:
-        data = pickle.load(fp)
-        id_lookup = {}
-        name_lookup = {}
-        for item in data:
-            id_lookup[item['id']] = item
-            name_lookup[item['name'].lower()] = item
-    return id_lookup, name_lookup
 
 def send(payload, update, context):
     chat_id = update.effective_message.chat_id
