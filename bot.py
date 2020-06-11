63d8fca66cb243bb34ebe1904dbac080afd8dcf8
Yossi Rafelson <yossi.rafelson@gmail.com>
2020-06-11 01:01:36 -0700

    Share user's last config before clearing

--- bot.py
+++ bot.py
@@ -271,9 +271,16 @@ def setting_saver(update, context, section):
 
 
 def setting_clear(update, context, section):
-    # TODO: Share users last config before clearing
+    old_settings = []
+    for item, count in sorted(context.user_data.get(section, {}).items()):
+        if count:
+            old_settings.append(f'{item},{count}')
+        else:
+            old_settings.append(item)
+
+    previous = f'\n Previously <code>/{section} {" ".join(old_settings)}</code>' if old_settings else ''
+    send(f'Ok, your {section} settings have been cleared{previous}', update, context)
     context.user_data[section] = {}
-    send(f'Ok, your {section} settings have been cleared', update, context)
 
 
 @send_typing_action
