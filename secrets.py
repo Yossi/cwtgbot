2f4ff6175d4d0070b0f8fc0e941af26600e9a8d4
Lola <lola@egherman.com>
2020-06-23 16:56:18 -0500

    use environ variables in secrets.py

---
+++ secrets.py
@@ -0,0 +1,4 @@
+import os
+import json
+TOKEN = os.getenv('TOKEN', default='bot token you got from BotFather')  # Set default if you do not plan to use ENVs
+LIST_OF_ADMINS = json.loads(os.getenv('LIST_OF_ADMINS', default='["numericalid1", "numericalid2"]'))
