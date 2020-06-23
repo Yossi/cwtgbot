515014dfa9aa66670cea6ad65cf4cc6f4ad73320
Lola <lola@egherman.com>
2020-06-23 15:58:15 -0500

    add warehouse to init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
 from .get_id_location import get_id_location
 from .get_qualifications import get_qualifications
 from .send import send
+from .warehouse import load_saved
 from .wiki import get_lookup_dicts, scrape_data, id_lookup, name_lookup
