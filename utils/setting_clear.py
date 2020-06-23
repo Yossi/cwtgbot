b9a187582e0881b43e90e7bd62e6582edbbb7556
Lola <lola@egherman.com>
2020-06-23 18:28:45 -0500

    make setting saver and setting clear work

--- utils/setting_clear.py
+++ utils/setting_clear.py
@@ -1,10 +1,9 @@
-import commands.user_data
 from utils import send
 
 
 def setting_clear(update, context, section):
     old_settings = []
-    for item, count in sorted(commands.user_data.user_data.get(section, {}).items()):
+    for item, count in sorted(context.user_data.get(section, {}).items()):
         if count:
             old_settings.append(f'{item},{count}')
         else:
@@ -12,4 +11,4 @@ def setting_clear(update, context, section):
 
     previous = f'\n Previously <code>/{section} {" ".join(old_settings)}</code>' if old_settings else ''
     send(f'Ok, your {section} settings have been cleared{previous}', update, context)
-    commands.user_data.user_data[section] = {}
+    context.user_data[section] = {}
--- utils/setting_saver.py
+++ utils/setting_saver.py
@@ -1,18 +1,17 @@
-import commands.user_data
 from utils import id_lookup, send
 
 
 def setting_saver(update, context, section):
     if context.args:
-        commands.user_data.user_data[section] = {}
+        context.user_data[section] = {}
     for id in context.args:
         count = ''
         if ',' in id:
             id, count = id.split(',')
         if id_lookup.get(id, {}).get('name', False):  # Don't save if item doesn't exist
-            commands.user_data.user_data[section][id] = count
+            context.user_data[section][id] = count
 
-    settings = sorted(commands.user_data.user_data.get(section, {}))
+    settings = sorted(context.user_data.get(section, {}))
     if len(settings) > 0:
         res = [f'{"Saving" if section == "save" else "Ignoring"} {"these" if len(settings) > 1 else "this"}:']
     else:
@@ -20,7 +19,7 @@ def setting_saver(update, context, section):
     cmd = [f'/{section}']
     for id in settings:
         name = id_lookup.get(id, {}).get('name', 'unknown')
-        count = commands.user_data.user_data[section][id]
+        count = context.user_data[section][id]
         id_count = f'{id}{"," if count else ""}{count}'
         res.append(f' <code>{id_count}</code> {name}')
         cmd.append(id_count)
