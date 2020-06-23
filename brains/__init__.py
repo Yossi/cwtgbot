de03e886d2b8a42d0ec98dbc73ac9b5e5947c55a
Lola <lola@egherman.com>
2020-06-23 16:47:06 -0500

    add inits to brains module

---
+++ brains/__init__.py
@@ -0,0 +1,5 @@
+from .deals_report import deals_report
+from .list import alch, misc, other, stock
+from .main import main
+from .warehouse_crafting import warehouse_crafting
+from .withdraw import parts, craft
