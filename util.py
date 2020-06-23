a75a1bf1120310d7d0a3a023f6f243e4cac46282
Lola <lola@egherman.com>
2020-06-23 15:51:19 -0500

    move game_phase to timewiz

--- util.py
+++ util.py
@@ -18,12 +18,7 @@ from tealeyes import CW_OFFSET, CW_PERIODS, SPEED
 
 
 
-def game_phase():
-    adjustment = -37.0
-    utcdt = datetime.now(timezone.utc)
-    cwtdt = datetime.fromtimestamp(SPEED * (utcdt.timestamp() + CW_OFFSET), tz=timezone.utc)
-    cwadt = datetime.fromtimestamp(cwtdt.timestamp() + SPEED * adjustment, tz=timezone.utc)
-    return f'{CW_PERIODS[cwadt.hour//6]}'
+
 
 
 def get_id_location(id):
