b97adc1a0a91631a9b4cc465a0141c5386b4bd04
Lola <lola@egherman.com>
2020-06-23 18:06:35 -0500

    add commands.deals

---
+++ commands/deals.py
@@ -0,0 +1,9 @@
+from brains import deals_report
+from utils import send
+from .decorators import log, send_typing_action
+
+@send_typing_action
+@log
+def deals(update, context):
+    response = deals_report(context)
+    send(response, update, context)
