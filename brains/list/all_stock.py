8eceda7adf8f31b4fcd5f28b2db7b63b0e323e34
Yossi Rafelson <yossi.rafelson@gmail.com>
2020-07-13 00:55:33 -0700

    Color the bars in /all_stock chart by where they came from

--- brains/list/all_stock.py
+++ brains/list/all_stock.py
@@ -1,10 +1,20 @@
 import datetime
 import io
 import json
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 
 from utils.warehouse import load_saved
 from utils.wiki import id_lookup
+
+color_lookup = {
+    'res': 'xkcd:red',
+    'rec': 'xkcd:brown',
+    'alch': 'xkcd:green',
+    'misc': 'xkcd:blue',
+    'parts': 'xkcd:orange',
+    'other': 'xkcd:violet'
+}
 
 def all_stock(context):
     warehouse = load_saved(guild=context.user_data.get('guild', ''))
@@ -36,28 +46,35 @@ def all_stock(context):
         output = [f'Based on {command} data {age.seconds // 60} minutes old:\n']
 
         items_by_weight = {}
+        items_by_category = {}
         for category in warehouse:
             if category != 'other':
                 for id, count in warehouse[category]['data'].items():
                     items_by_weight[id] = count * id_lookup.get(id, {}).get('weight', 0)
+                    items_by_category[id] = category
             else:
                 for id in warehouse[category]['data']:
                     items_by_weight[id] = sum(list(zip(*warehouse[category]['data'][id]))[2]) * id_lookup.get(id, {}).get('weight', 0)
+                    items_by_category[id] = category
 
         total_guild_weight = sum(items_by_weight.values())
         output.append(f'Total guild weight: {total_guild_weight}')
 
-        sorted_by_weight = sorted(items_by_weight, key=items_by_weight.get, reverse=True)
+        sorted_by_weight = sorted(items_by_weight, key=items_by_weight.get, reverse=False)
 
         x = [items_by_weight[id] for id in sorted_by_weight]
         y = [f"{id_lookup.get(id, {'name': 'unidentified object'})['name'].lower()} {id}" for id in sorted_by_weight]
         r = range(len(x))
+        colors = [color_lookup[items_by_category[item.rpartition(' ')[2]]] for item in y]
 
         plt.clf()  # clear plot, because it doesn't get cleared from last run
         plt.figure(figsize=(6.4, (len(x) * 0.15)))
-        plt.barh(r, x)
+        plt.barh(r, x, color=colors)
         plt.yticks(r, y, fontsize='8', fontname='symbola')
-        plt.legend(loc='upper right', labels=['Weight in Guild Stock'])
+        # plt.set_ticks_position('top')
+        plt.title('Weight in Guild Stock')
+        patches = [mpatches.Patch(color=color, label=category) for category, color in color_lookup.items()]
+        plt.legend(handles=patches, ncol=3, title='/g_stock_…')
         plt.subplots_adjust(left=0.3)
         buf = io.BytesIO()
         buf.name = 'weights.pdf'
