dd56ea9748857c27a68e942b68eafed4e7af784c
Yossi Rafelson <yossi.rafelson@gmail.com>
2020-08-21 03:09:12 -0700

    Add some jank code that tries to avoid splitting long messages mid tag

--- utils/send.py
+++ utils/send.py
@@ -1,14 +1,32 @@
 import logging
-
+import re
 import filetype
 from telegram import ParseMode
 
 
 def send(payload, update, context):
+    n = 100 # number of formatted entities tg allows per message
     chat_id = update.effective_message.chat_id
     if isinstance(payload, str):
         max_size = 4096
-        for text in [payload[i:i + max_size] for i in range(0, len(payload), max_size)]:
+        i = 0
+        while i < len(payload):
+            text = payload[i:i + max_size]
+            open_tags = text.count('<')
+            consumed_chars = max_size
+            if open_tags != text.count('>'): # cut mid tag
+                consumed_chars = text.rfind('<')
+                text = text[:consumed_chars]
+                open_tags -= 1
+            if open_tags % 2: # close tag missing
+                consumed_chars = text.rfind('<')
+                text = text[:consumed_chars]
+                open_tags -= 1
+            if open_tags > 2 * n: # too many tag pairs
+                consumed_chars = [m.start() for m in re.finditer('<', text)][2 * n]
+                text = text[:consumed_chars]
+            i = i + consumed_chars
+
             logging.info(f'bot said:\n{text}')
             context.bot.send_message(chat_id=chat_id, text=text, parse_mode=ParseMode.HTML, disable_web_page_preview=True)
     else:
