d5552ed0ab80c090fcc4b410fba4dfcac81362a1
Yossi Rafelson <yossi.rafelson@gmail.com>
2020-08-21 17:27:36 -0700

    Sort smarter

--- brains/prices_report.py
+++ brains/prices_report.py
@@ -1,7 +1,19 @@
 import datetime
 import pickle
+import re
 
 from utils.wiki import name_lookup
+
+
+def special_sort(x):
+    m = re.search(r'>(\D*)(\d*)<', x)
+    prefix = m.groups()[0]
+    num = ''
+    try:
+        num = int(m.groups()[1])
+    except:
+        pass
+    return prefix, num
 
 
 def prices_report(context):
@@ -29,7 +41,7 @@ def prices_report(context):
                 continue
 
             out_str = f"<code>{id}</code> {data['name']} {'💰' if data.get('exchange') else '👝'}{price}"
-            output.append(out_str)  # \"{data['recipe']}\"
-    output.sort() # would be nice to do something other than dumb alphabetical here
+            output.append(out_str)
+    output.sort(key = special_sort)
     output.append(f"\nPrices no fresher than {(now - prices['last_update']).seconds // 60} minutes.")
     return '\n'.join(output)
