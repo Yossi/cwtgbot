a619e6719acee2d206a11a9b9bcf984e546b840b
Yossi Rafelson <yossi.rafelson@gmail.com>
2020-09-01 10:50:53 -0700

    Fix mysterious copy/paste error

--- commands/location.py
+++ commands/location.py
@@ -10,6 +10,6 @@ def location(update, context):
     tf = TimezoneFinder()
     latitude, longitude = update.effective_message.location.latitude, update.effective_message.location.longitude
     context.user_data['location'] = round(latitude, 3), round(longitude, 3)
-    context.user_data.user_data['timezone'] = tf.timezone_at(lat=latitude, lng=longitude)
+    context.user_data['timezone'] = tf.timezone_at(lat=latitude, lng=longitude)
     text = f'Saving your location as {context.user_data["location"]} making your timezone be {context.user_data["timezone"]}'
     send(text, update, context)
